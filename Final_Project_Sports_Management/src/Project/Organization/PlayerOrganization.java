b764767cc0ab36d0136ee74dc0b5b9867e421e42
vidish-kale <kale.v@northeastern.edu>
2022-12-08 13:59:25 -0500

    Modules added

--- Final_Project_Sports_Management/src/Project/Organization/PlayerOrganization.java
+++ Final_Project_Sports_Management/src/Project/Organization/PlayerOrganization.java
@@ -4,10 +4,38 @@
  */
 package Project.Organization;
 
+import Project.Player.PlayerDirectory;
+import Project.Role.PlayerRole;
+import Project.Role.Role;
+import java.util.HashSet;
+
 /**
  *
  * @author vidis
  */
-public class PlayerOrganization {
+public class PlayerOrganization extends Organization{
     
+    PlayerDirectory playerlist;
+    //constructor to add event maker list to Event Maker directory
+    public PlayerOrganization()
+    {
+        super(Organization.Type.Player.getValue());
+        playerlist= new PlayerDirectory();
+    }
+
+    public PlayerDirectory getPlayerlist() {
+        return playerlist;
+    }
+
+    public void setPlayerlist(PlayerDirectory playerlist) {
+        this.playerlist = playerlist;
+    }
+    
+    //hash map to support event maker role
+    @Override
+    public HashSet<Role> getSupportedRole(){
+       role.add(new PlayerRole());
+       return role;
+       
+    }
 }
