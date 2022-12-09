f30f2c7b9647fd3bb1ac19a3b7e2e1780476dc67
Prathmesh Pardeshi <pardeshi.pr@northeastern.edu>
2022-12-08 19:23:44 -0500

    Added Scheduling commitee role and Landing JFrame

---
+++ Final_Project_Sports_Management/src/Project/NGO/NGODirectory.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project.NGO;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rajen
+ */
+public class NGODirectory {
+    private ArrayList<NGO> ngodirectory;
+
+    public NGODirectory() {
+    ngodirectory= new ArrayList<>();
+    }
+
+    public ArrayList<NGO> getNgodirectory() {
+        return ngodirectory;
+    }
+
+    public void setNgodirectory(ArrayList<NGO> ngodirectory) {
+        this.ngodirectory = ngodirectory;
+    }
+    //add a NGO
+    public NGO addNGO(){
+        NGO ngo =new NGO();
+        ngodirectory.add(ngo);
+        return ngo;
+    }
+    //remove a NGO
+    public void removeNGO(NGO ngo){
+        ngodirectory.remove(ngo);   
+    }
+}
