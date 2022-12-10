74682e902f8a51ed8f8069e44c773f0fdb0627c9
vidish-kale <kale.v@northeastern.edu>
2022-12-10 13:43:14 -0500

    Added Coach and Coach Admin Role

---
+++ Final_Project_Sports_Management/src/Project/Role/ProviderAdminRole.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project.Role;
+
+import Project.MainSystem;
+import Project.Enterprise.Enterprise;
+import Project.Organization.Organization;
+import Project.UserAccount.UserAccount;
+import UI.ProviderEnterprise.ProviderAdminWorkArea;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author vidis
+ */
+public class ProviderAdminRole extends Role{
+    //overriding main panel with provier admin work area whenever provider admin role is called
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,MainSystem system){
+        return new ProviderAdminWorkArea(userProcessContainer,enterprise,system);
+    }   
+        @Override
+        public String toString()
+{
+   return Role.RoleType.ProviderAdminRole.getValue();
+}
+}
---
+++ Final_Project_Sports_Management/src/Project/Role/ProviderRole.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project.Role;
+
+import Project.MainSystem;
+import Project.Enterprise.Enterprise;
+import Project.Organization.Organization;
+import Project.Organization.ProviderOrganization;
+import Project.UserAccount.UserAccount;
+import UI.Provider.ProviderWorkArea;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author vidis
+ */
+public class ProviderRole extends Role{
+    //overriding main panel with provider work area whenever provider role is called
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,MainSystem system){
+        return new ProviderWorkArea(userProcessContainer,account,(ProviderOrganization)organization,enterprise,system);
+    }
+        @Override
+        public String toString()
+{
+   return Role.RoleType.ProviderRole.getValue();
+}
+
+
+}
