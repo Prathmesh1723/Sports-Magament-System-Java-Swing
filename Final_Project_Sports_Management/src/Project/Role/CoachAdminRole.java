d50eb7a92497af9d435326db449832b5c89758ae
vidish-kale <kale.v@northeastern.edu>
2022-12-10 11:38:27 -0500

    Added Coach and Coach Admin Role

--- Final_Project_Sports_Management/src/Project/Role/CoachAdminRole.java
+++ Final_Project_Sports_Management/src/Project/Role/CoachAdminRole.java
@@ -9,24 +9,23 @@ import Project.MainSystem;
 import Project.Enterprise.Enterprise;
 import Project.Organization.Organization;
 import Project.UserAccount.UserAccount;
-import UI.SelectorEnterprise.SelectorAdminWorkArea;
+import UI.CoachEnterprise.CoachAdminWorkArea;
 import javax.swing.JPanel;
 
 /**
  *
- * @author prath
+ * @author vidis
  */
 public class CoachAdminRole extends Role{
- 
-	//overriding main panel with doctor admin work area whenever doctor admin role is called
+    //overriding main panel with police admin work area whenever police admin role is called
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,MainSystem system){
-        return new SelectorAdminWorkArea(userProcessContainer,enterprise,system);
+        return new CoachAdminWorkArea(userProcessContainer,enterprise,system);
     }   
+        @Override
+        public String toString()
+{
+   return Role.RoleType.CoachAdminRole.getValue();
+}
 
-    @Override
-    public String toString()
-    {
-    	return Role.RoleType.DoctorAdminRole.getValue();
-    }
 }
--- Final_Project_Sports_Management/src/Project/Role/CoachRole.java
+++ Final_Project_Sports_Management/src/Project/Role/CoachRole.java
@@ -7,27 +7,26 @@ package Project.Role;
 
 import Project.MainSystem;
 import Project.Enterprise.Enterprise;
-import Project.Organization.CoachOrganization;
 import Project.Organization.Organization;
+import Project.Organization.CoachOrganization;
 import Project.UserAccount.UserAccount;
-import UI.EventWorkArea.EventMakerWorkArea;
+import UI.CoachWorkArea.CoachWorkArea;
 import javax.swing.JPanel;
 
 /**
  *
- * @author prath
+ * @author vidis
  */
 public class CoachRole extends Role{
- 
-	//overriding main panel with doctore work area whenever doctor role is called
+    //overriding main panel with police work area whenever police role is called
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,MainSystem system){
-        return new EventMakerWorkArea(userProcessContainer,account,(CoachOrganization)organization,enterprise,system);
+        return new CoachWorkArea(userProcessContainer,account,(CoachOrganization)organization,enterprise,system);
     }
+        @Override
+        public String toString()
+{
+   return Role.RoleType.CoachRole.getValue();
+}
 
-    @Override
-    public String toString()
-    {
-    	return Role.RoleType.Doctor.getValue();
-    }
 }
