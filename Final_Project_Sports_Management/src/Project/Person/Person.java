f26dafac123b9c048120a3f46f94fd562727f545
vidish-kale <kale.v@northeastern.edu>
2022-12-10 09:47:50 -0500

    Updated Person Directory

---
+++ Final_Project_Sports_Management/src/Project/Person/Person.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project.Person;
+
+/**
+ *
+ * @author prath
+ */
+public class Person {
+    
+    private String personName;
+    private int personID;
+    private static int count=1;
+    
+    public Person(){
+        personID=count;
+        count++;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(int id) {
+        this.personID = id;
+    }
+    
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String name) {
+        this.personName = name;
+    }
+    @Override
+    public String toString(){
+        return personName;
+    }
+    
+}
